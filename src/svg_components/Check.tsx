c2f651c18fc4bc214ad4394a2f3a4a04368cc27f
arungit2020 <aruprajapati9825@gmail.com>
2024-02-17 17:46:18 +0530

    added svg components

---
+++ src/svg_components/Check.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+const SvgCheck = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      stroke={props.stroke}
+      strokeLinecap="round"
+      strokeWidth={props.strokeWidth || 2}
+      d="m4 12.314 5.657 5.656L20.97 6.657"
+    />
+  </svg>
+);
+export default SvgCheck;
---
+++ src/svg_components/Ellipse.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+const SvgEllipse = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 9 8"
+    {...props}
+  >
+    <circle cx={4.5} cy={4} r={4} fill={props.fill} />
+  </svg>
+);
+export default SvgEllipse;
