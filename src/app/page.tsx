26495f9cb4f95999cba27da8d25e68537f92a415
Arun Kumar <arunprajapati9825@gmail.com>
2024-04-03 21:32:31 +0530

    fix the responsive issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { Hero } from '../components/Hero/Hero';
 import { OurStats } from '@/components/Hero/OurStats';
-import TrustedOrgList from '@/components/Hero/TrustedOrgList';
 
 export default async function Home() {
   return (
     <main className="mx-8 my-8 dark:bg-[#070815] bg-gray-900/50 h-dvh ">
       <Hero />
       <OurStats />
-      <TrustedOrgList />
+      {/* <TrustedOrgList /> */}
     </main>
   );
 }
--- src/components/ResponsiveWrapper.tsx
+++ src/components/ResponsiveWrapper.tsx
@@ -11,10 +11,10 @@ export const ResponsiveWrapper = async ({ children }: Props) => {
   return (
     <main className="absolute inset-x-0 flex justify-center items-center">
       <div className="w-full min-h-dvh">
-        <div className="fixed  min-w-full z-50 md:top-2 md:mb-2 ">
+        <div className="fixed  min-w-full z-50 md:top-2 md:mb-7 ">
           <NavBar />
         </div>
-        <div className="min-h-full top-8 md:top-[5rem] ml-0 md:ml-32 mr-0 md:mr-32 md:px-8 md:py-2 -px-2 py-3 ">
+        <div className="relative min-h-full top-8 md:top-[5rem] ml-0 md:ml-32 mr-0 md:mr-32 md:px-8 md:py-2 -px-2 py-3 ">
           {children}
         </div>
       </div>
